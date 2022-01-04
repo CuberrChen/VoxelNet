c47631b033a6e327f251701ec071f3cb9eec20fa
xbchen <44107003+CuberrChen@users.noreply.github.com>
2022-01-04 20:54:15 +0800

    fix mgpu train function: remove eval part

--- voxelnet/pypaddle/train_mgpu.py
+++ voxelnet/pypaddle/train_mgpu.py
@@ -11,16 +11,12 @@ import paddle
 from visualdl import LogWriter as SummaryWriter
 
 import paddleplus
-import voxelnet.data.kitti_common as kitti
 from voxelnet.configs import cfg_from_config_py_file
 from voxelnet.builder import target_assigner_builder, voxel_builder
 from voxelnet.data.preprocess import merge_voxelnet_batch
 from voxelnet.pypaddle.builder import (box_coder_builder, input_reader_builder,
                                        lr_scheduler_builder, optimizer_builder,
                                        voxelnet_builder)
-from voxelnet.utils.eval import get_coco_eval_result, get_official_eval_result
-from voxelnet.utils.progress_bar import ProgressBar
-
 
 def _get_pos_neg_loss(cls_loss, labels):
     # cls_loss: [N, num_anchors, num_class]
@@ -179,12 +175,6 @@ def train(config_path,
         training=True,
         voxel_generator=voxel_generator,
         target_assigner=target_assigner)
-    eval_dataset = input_reader_builder.build(
-        eval_input_cfg,
-        model_cfg,
-        training=False,
-        voxel_generator=voxel_generator,
-        target_assigner=target_assigner)
 
     def _worker_init_fn(worker_id):
         time_seed = np.array(time.time(), dtype=np.int32)
@@ -194,23 +184,12 @@ def train(config_path,
     batch_sampler = paddle.io.DistributedBatchSampler(
         dataset, batch_size=input_cfg.batch_size, shuffle=True, drop_last=False)
 
-    eval_batch_sampler = paddle.io.DistributedBatchSampler(
-        dataset, batch_size=eval_input_cfg.batch_size, shuffle=False, drop_last=False)
-
     dataloader = paddle.io.DataLoader(
         dataset=dataset,
         batch_sampler=batch_sampler,
         num_workers=input_cfg.num_workers,
         collate_fn=merge_voxelnet_batch,
         worker_init_fn=_worker_init_fn,
-        use_shared_memory=False,
-        persistent_workers=True,
-    )
-    eval_dataloader = paddle.io.DataLoader(
-        dataset=eval_dataset,
-        batch_sampler=eval_batch_sampler,
-        num_workers=eval_input_cfg.num_workers,
-        collate_fn=merge_voxelnet_batch,
         use_shared_memory=False,
         persistent_workers=True,
     )
@@ -318,9 +297,6 @@ def train(config_path,
                         cls_pos_loss.detach().cpu().numpy())
                     metrics["loss"]["cls_neg_rt"] = float(
                         cls_neg_loss.detach().cpu().numpy())
-                    # if unlabeled_training:
-                    #     metrics["loss"]["diff_rt"] = float(
-                    #         diff_loc_loss_reduced.detach().cpu().numpy())
                     if model_cfg.use_direction_classifier:
                         metrics["loss"]["dir_rt"] = float(
                             dir_loss_reduced.detach().cpu().numpy())
@@ -360,76 +336,16 @@ def train(config_path,
                     print(log_str, file=logf)
                     print(log_str)
                 ckpt_elasped_time = time.time() - ckpt_start_time
-                if ckpt_elasped_time > train_cfg.save_checkpoints_secs:
+                if ckpt_elasped_time > train_cfg.save_checkpoints_secs and local_rank == 0:
                     paddleplus.train.save_models(model_dir, [net, optimizer],
                                                  net.get_global_step())
                     ckpt_start_time = time.time()
 
-                # paddle.device.cuda.empty_cache() # 清空显存 防止溢出
-
+            # paddle.device.cuda.empty_cache() # 清空显存 防止溢出
             total_step_elapsed += steps
-            paddleplus.train.save_models(model_dir, [net, optimizer],
-                                         net.get_global_step())
-            net.eval()
-            result_path_step = result_path / f"step_{net.get_global_step()}"
-            result_path_step.mkdir(parents=True, exist_ok=True)
-            print("#################################")
-            print("#################################", file=logf)
-            print("# EVAL")
-            print("# EVAL", file=logf)
-            print("#################################")
-            print("#################################", file=logf)
-            print("Generate output labels...")
-            print("Generate output labels...", file=logf)
-            t = time.time()
-            dt_annos = []
-            prog_bar = ProgressBar()
-            prog_bar.start(len(eval_dataset) // eval_input_cfg.batch_size + 1)
-            for example in iter(eval_dataloader):
-                example = example_convert_to_paddle(example, float_dtype)
-                if pickle_result:
-                    dt_annos += predict_kitti_to_anno(
-                        net, example, class_names, center_limit_range,
-                        model_cfg.lidar_input)
-                else:
-                    _predict_kitti_to_file(net, example, result_path_step,
-                                           class_names, center_limit_range,
-                                           model_cfg.lidar_input)
-
-                prog_bar.print_bar()
-
-            sec_per_ex = len(eval_dataset) / (time.time() - t)
-            print(f"avg forward time per example: {net.avg_forward_time:.3f}")
-            print(
-                f"avg postprocess time per example: {net.avg_postprocess_time:.3f}"
-            )
-
-            net.clear_time_metrics()
-            print(f'generate label finished({sec_per_ex:.2f}/s). start eval:')
-            print(
-                f'generate label finished({sec_per_ex:.2f}/s). start eval:',
-                file=logf)
-            gt_annos = [
-                info["annos"] for info in eval_dataset.dataset.kitti_infos
-            ]
-            if not pickle_result:
-                dt_annos = kitti.get_label_annos(result_path_step)
-            if paddle.device.cuda.device_count() == 0:
-                print("Evaluation only support gpu.")
-            else:
-                result = get_official_eval_result(gt_annos, dt_annos, class_names)
-                print(result, file=logf)
-                print(result)
-                writer.add_text('eval_result', result, global_step)
-                result = get_coco_eval_result(gt_annos, dt_annos, class_names)
-                print(result, file=logf)
-                print(result)
-                writer.add_text('eval_result', result, global_step)
-            if pickle_result:
-                with open(result_path_step / "result.pkl", 'wb') as f:
-                    pickle.dump(dt_annos, f)
-
-            net.train()
+            if local_rank == 0:
+                paddleplus.train.save_models(model_dir, [net, optimizer],
+                                             net.get_global_step())
     except Exception as e:
         paddleplus.train.save_models(model_dir, [net, optimizer],
                                      net.get_global_step())
@@ -439,137 +355,6 @@ def train(config_path,
     paddleplus.train.save_models(model_dir, [net, optimizer],
                                  net.get_global_step())
     logf.close()
-
-
-def _predict_kitti_to_file(net,
-                           example,
-                           result_save_path,
-                           class_names,
-                           center_limit_range=None,
-                           lidar_input=False):
-    batch_image_shape = example['image_shape']
-    batch_imgidx = example['image_idx']
-    predictions_dicts = net(example)
-    # t = time.time()
-    for i, preds_dict in enumerate(predictions_dicts):
-        image_shape = batch_image_shape[i]
-        img_idx = preds_dict["image_idx"]
-        if preds_dict["bbox"] is not None:
-            box_2d_preds = preds_dict["bbox"].cpu().numpy()
-            box_preds = preds_dict["box3d_camera"].cpu().numpy()
-            scores = preds_dict["scores"].cpu().numpy()
-            box_preds_lidar = preds_dict["box3d_lidar"].cpu().numpy()
-            # write pred to file
-            box_preds = box_preds[:, [0, 1, 2, 4, 5, 3,
-                                      6]]  # lhw->hwl(label file format)
-            label_preds = preds_dict["label_preds"].cpu().numpy()
-            # label_preds = np.zeros([box_2d_preds.shape[0]], dtype=np.int32)
-            result_lines = []
-            for box, box_lidar, bbox, score, label in zip(
-                    box_preds, box_preds_lidar, box_2d_preds, scores,
-                    label_preds):
-                if not lidar_input:
-                    if bbox[0] > image_shape[1] or bbox[1] > image_shape[0]:
-                        continue
-                    if bbox[2] < 0 or bbox[3] < 0:
-                        continue
-                # print(img_shape)
-                if center_limit_range is not None:
-                    limit_range = np.array(center_limit_range)
-                    if (np.any(box_lidar[:3] < limit_range[:3])
-                            or np.any(box_lidar[:3] > limit_range[3:])):
-                        continue
-                bbox[2:] = np.minimum(bbox[2:], image_shape[::-1])
-                bbox[:2] = np.maximum(bbox[:2], [0, 0])
-                result_dict = {
-                    'name': class_names[int(label)],
-                    'alpha': -np.arctan2(-box_lidar[1], box_lidar[0]) + box[6],
-                    'bbox': bbox,
-                    'location': box[:3],
-                    'dimensions': box[3:6],
-                    'rotation_y': box[6],
-                    'score': score,
-                }
-                result_line = kitti.kitti_result_line(result_dict)
-                result_lines.append(result_line)
-        else:
-            result_lines = []
-        result_file = f"{result_save_path}/{kitti.get_image_index_str(img_idx)}.txt"
-        result_str = '\n'.join(result_lines)
-        with open(result_file, 'w') as f:
-            f.write(result_str)
-
-
-def predict_kitti_to_anno(net,
-                          example,
-                          class_names,
-                          center_limit_range=None,
-                          lidar_input=False,
-                          global_set=None):
-    batch_image_shape = example['image_shape']
-    batch_imgidx = example['image_idx']
-    predictions_dicts = net(example)
-    # t = time.time()
-    annos = []
-    for i, preds_dict in enumerate(predictions_dicts):
-        image_shape = batch_image_shape[i].cpu().numpy()
-        img_idx = preds_dict["image_idx"]
-        if preds_dict["bbox"] is not None:
-            box_2d_preds = preds_dict["bbox"].detach().cpu().numpy()
-            box_preds = preds_dict["box3d_camera"].detach().cpu().numpy()
-            scores = preds_dict["scores"].detach().cpu().numpy()
-            box_preds_lidar = preds_dict["box3d_lidar"].detach().cpu().numpy()
-            # write pred to file
-            label_preds = preds_dict["label_preds"].detach().cpu().numpy()
-            # label_preds = np.zeros([box_2d_preds.shape[0]], dtype=np.int32)
-            anno = kitti.get_start_result_anno()
-            num_example = 0
-            for box, box_lidar, bbox, score, label in zip(
-                    box_preds, box_preds_lidar, box_2d_preds, scores,
-                    label_preds):
-                if not lidar_input:
-                    if bbox[0] > image_shape[1] or bbox[1] > image_shape[0]:
-                        continue
-                    if bbox[2] < 0 or bbox[3] < 0:
-                        continue
-                # print(img_shape)
-                if center_limit_range is not None:
-                    limit_range = np.array(center_limit_range)
-                    if (np.any(box_lidar[:3] < limit_range[:3])
-                            or np.any(box_lidar[:3] > limit_range[3:])):
-                        continue
-                bbox[2:] = np.minimum(bbox[2:], image_shape[::-1])
-                bbox[:2] = np.maximum(bbox[:2], [0, 0])
-                anno["name"].append(class_names[int(label)])
-                anno["truncated"].append(0.0)
-                anno["occluded"].append(0)
-                anno["alpha"].append(-np.arctan2(-box_lidar[1], box_lidar[0]) +
-                                     box[6])
-                anno["bbox"].append(bbox)
-                anno["dimensions"].append(box[3:6])
-                anno["location"].append(box[:3])
-                anno["rotation_y"].append(box[6])
-                if global_set is not None:
-                    for i in range(100000):
-                        if score in global_set:
-                            score -= 1 / 100000
-                        else:
-                            global_set.add(score)
-                            break
-                anno["score"].append(score)
-
-                num_example += 1
-            if num_example != 0:
-                anno = {n: np.stack(v) for n, v in anno.items()}
-                annos.append(anno)
-            else:
-                annos.append(kitti.empty_result_anno())
-        else:
-            annos.append(kitti.empty_result_anno())
-        num_example = annos[-1]["name"].shape[0]
-        annos[-1]["image_idx"] = np.array(
-            [img_idx] * num_example, dtype=np.int64)
-    return annos
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Model training')
@@ -612,12 +397,6 @@ def parse_args():
         help='do grad accum..',
         default=1,
         type=int)
-    parser.add_argument(
-        '--pickle_result',
-        dest='pickle_result',
-        help='Whther to use pickle_result for eval',
-        default=True,
-        type=bool)
     return parser.parse_args()
 
 
@@ -629,7 +408,6 @@ def main(args):
           display_step=args.display_step,
           summary_step=args.summary_step,
           accum_step=args.accum_step,
-          pickle_result=args.pickle_result
           )
 
 if __name__ == '__main__':
